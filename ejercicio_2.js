196b2fdf20457d99a76e58aeb2e842ce905911dc
David Rueda <Rueda9673@gmail.com>
2025-09-29 17:55:18 -0500

    Se realizo el ejercicio numero dos

---
+++ ejercicio_2.js
@@ -0,0 +1,6 @@
+let numero_horas = parseInt(prompt("Ingrese el número de horas trabajadas: ")); //Se solicita el número de horas trabajadas
+let valor_hora = parseInt(prompt("Ingrese el valor por hora: ")); //Se solicita el valor por hora
+
+let calculo_salario_semanal = numero_horas * valor_hora; //Se calcula el salario semanal
+
+alert("El salario semanal es de: " + calculo_salario_semanal); //Se muestra el resultado
