1b556bedfd7a2dbaf054939454a9e2daee0847c8
David Rueda <Rueda9673@gmail.com>
2025-09-29 17:55:42 -0500

    Se realizo el ejercicio numero tres

---
+++ ejercicio_3.js
@@ -0,0 +1,8 @@
+let edad1 = parseInt(prompt("Ingrese la edad de la primera persona:")); //Se solicita la edad de la primera persona
+let edad2 = parseInt(prompt("Ingrese la edad de la segunda persona:")); //Se solicita la edad de la segunda persona
+let edad3 = parseInt(prompt("Ingrese la edad de la tercera persona:")); //Se solicita la edad de la tercera persona
+let edad4 = parseInt(prompt("Ingrese la edad de la cuarta persona:")); //Se solicita la edad de la cuarta persona
+
+let promedio = (edad1 + edad2 + edad3 + edad4) / 4; //Se calcula el promedio de edad
+
+alert("El promedio de edad es: " + promedio); //Se muestra el resultado
